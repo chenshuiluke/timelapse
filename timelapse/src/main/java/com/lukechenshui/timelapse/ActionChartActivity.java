b639db3765d23c36570fa2f9a0a7242821aac918
Luke Chen Shui <chenshuiluke@gmail.com>
2016-10-03 14:11:21 -0500

    Improved the chart

--- timelapse/src/main/java/com/lukechenshui/timelapse/ActionChartActivity.java
+++ timelapse/src/main/java/com/lukechenshui/timelapse/ActionChartActivity.java
@@ -1,40 +1,95 @@
 package com.lukechenshui.timelapse;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.AxisValueFormatter;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 public class ActionChartActivity extends AppCompatActivity {
     String actionName = "";
-
+    LineChart chart;
+    int previousVal = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_chart);
         ArrayList<Action> actions = getIntent().getParcelableArrayListExtra("actions");
+        final HashMap<Integer, Action> actionHashMap = new HashMap<>();
+        chart = (LineChart) findViewById(R.id.chart);
         Collections.reverse(actions);
         actionName = actions.size() > 0 ? actions.get(0).getName() : "";
 
         List<Entry> entries = new ArrayList<>();
-
+        Log.i("Charting", "Number of actions: " + String.valueOf(actions.size()));
         for (int counter = 0; counter < actions.size(); counter++) {
             Action action = actions.get(counter);
             entries.add(new Entry(counter, action.getDuration()));
+            actionHashMap.put(counter, action);
         }
 
         LineDataSet dataSet = new LineDataSet(entries, actionName + " Times");
         LineData data = new LineData(dataSet);
-        LineChart chart = (LineChart) findViewById(R.id.chart);
+
+
+        dataSet.setLineWidth(1.75f);
+        dataSet.setCircleRadius(5f);
+        dataSet.setCircleHoleRadius(2.5f);
+        dataSet.setColor(Color.WHITE);
+        dataSet.setCircleColor(Color.WHITE);
+        dataSet.setHighLightColor(Color.WHITE);
+        dataSet.setDrawValues(false);
+
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setGranularity(1 / actions.size());
+        xAxis.setLabelCount(3, true);
+        xAxis.setTextColor(Color.WHITE);
+        xAxis.setTextSize(20);
+
+        chart.getAxisLeft().setTextColor(Color.WHITE);
+        chart.getAxisRight().setTextColor(Color.WHITE);
+
+        chart.getAxisLeft().setTextSize(20);
+        chart.getAxisRight().setTextSize(20);
+
+        xAxis.setValueFormatter(new AxisValueFormatter() {
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                Log.i("Charting", "Value: " + String.valueOf(value));
+                Action action = actionHashMap.get((int) value);
+                if (action != null) {
+                    return action.getFormattedStartDate();
+                } else {
+                    return "";
+                }
+
+            }
+
+            @Override
+            public int getDecimalDigits() {
+                return 0;
+            }
+        });
+        chart.setExtraOffsets(30, 10, 30, 0);
         chart.setData(data);
         chart.setDescription("Duration of action");
+        chart.setDescriptionTextSize(15);
+        chart.setDescriptionColor(Color.WHITE);
+        chart.setBackgroundColor(Color.rgb(250, 104, 104));
+
         chart.invalidate();
     }
 }
